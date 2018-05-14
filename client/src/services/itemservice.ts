25b4c30d19e108b6354879a6e1f759715a4e0ade
freddan962 <fredde.sander@gmail.com>
2018-05-14 16:23:49 +0200

    Initial unit support

--- client/src/services/itemservice.ts
+++ client/src/services/itemservice.ts
@@ -10,15 +10,28 @@ export class ItemService {
 
   public static initialize() : void {
     if (ItemService.initialized) return;
+
+    let items = [
+      { name: 'Cucumber', unit: 'kg', amount: 0.5, department: 6 },
+      { name: 'Iceberg lettuce', unit: 'piece', amount: 1, department: 6 },
+      { name: 'Cabbage', unit: 'pieces', amount: 2, department: 6 },
+      { name: 'Apples', unit: 'pieces', amount: 11, department: 6 },
+      { name: 'Tea', unit: 'box', amount: 1, department: 8 },
+      { name: 'Honey', unit: 'jars', amount: 2, department: 9 },
+    ]
     
-    this.addItem(new Item('Cucumber', false, DepartmentService.getByID(6)));
-    this.addItem(new Item('Iceberg lettuce', false, DepartmentService.getByID(6)));
-    this.addItem(new Item('Cabbage', false, DepartmentService.getByID(6)));
-    this.addItem(new Item('Apples', false, DepartmentService.getByID(6)));
-    this.addItem(new Item('Tea', false, DepartmentService.getByID(8)));
-    this.addItem(new Item('Honey', false, DepartmentService.getByID(9)));
-    
+    items.forEach((data) => {
+      this.initializeItem(data);
+    });
+
     ItemService.initialized = true;
+  }
+
+  public static initializeItem(itemData) {
+    let item = new Item(itemData.name, false, DepartmentService.getByID(itemData.department));
+    item.setUnit(itemData.unit);
+    item.setAmount(itemData.amount);
+    this.addItem(item);
   }
   
   public static getItems() : Item[] {
